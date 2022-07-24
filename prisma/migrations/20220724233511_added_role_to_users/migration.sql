148c130d142bf65cf608e1671a8775293575dcb4
Roger Urich <rogerurich@overtime.local>
2022-07-24 16:42:09 -0700

    Create migration.sql
    Added a new column to the user table

---
+++ prisma/migrations/20220724233511_added_role_to_users/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `role` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "role" "Role" NOT NULL;
+
+/* I ended up creating these updates manually. */

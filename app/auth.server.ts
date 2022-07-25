ccc8c599f7f1e55a0b4cdfda43e8796d772c8dbd
Roger Urich <rogerurich@overtime.local>
2022-07-24 18:24:53 -0700

    added validation function

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -2,6 +2,7 @@ import type { User } from "~/models/user.server";
 import { getAWSSecretsConfig } from '../app/utils'
 import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager'
 import jwt from 'jsonwebtoken'
+import invariant from "tiny-invariant";
 
 
 export async function SignJWT({ userId, role }: { userId: User["id"]; role: User["role"]; }): Promise<String> {
@@ -20,4 +21,20 @@ export async function SignJWT({ userId, role }: { userId: User["id"]; role: User
     }, response.SecretString as string, { algorithm: 'RS256', expiresIn: "1d" });
     
     return token;
+}
+
+export function ValidateJWT(token: string): Boolean {
+    const publicKey = process.env.JWT_SECRET
+    invariant(publicKey, "JWT Secret not set.");
+
+    jwt.verify(token, publicKey, (err, payload) => {
+        if (err) {
+            console.log('ERROR: ', err);
+            return false
+        }
+        
+    console.log('JWT is valid and payload is\n', payload);
+    });
+
+    return true
 }
